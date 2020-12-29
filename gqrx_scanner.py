eec17ccc2f6becc04946e7e1bd9adf8413b93d30
Starodubtsev Maksim <starodubtsevm@gmail.com>
2020-12-29 19:15:44 +0500

    реорганизован класс приемника

--- gqrx_scanner.py
+++ gqrx_scanner.py
@@ -4,12 +4,25 @@ import time
 
 class Receiver:
 
-    def __init__(self, hostname='127.0.0.1', port=7356, directory='/', waitTime=3, signalStrength=-31):
+    def __init__(self, hostname='127.0.0.1', port=7356, directory='/', waitTime=3,\
+                     signalStrength=-30, csvfile = 'freq.csv', sql = -31):
         self.host = hostname
         self.port = port
         self.directory = directory
         self.waitTime = waitTime
         self.signalStrength = signalStrength
+        self.csvfile = csvfile
+        self.freqs = {}
+        self.scan_start = False
+        self.sql = sql
+
+        freqs = {}
+        with open(self.csvfile, 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter = ',')
+            for row in reader:
+                if int(row[3]) == 1:
+                    freq = row[0]
+                    self.freqs[freq] = {"mode":row[1],"tag": row[2]}
 
     def _set_freq(self, freq):
         return self._update("F %s" % freq)
@@ -18,13 +31,47 @@ class Receiver:
         return self._update("M %s" % mode)
 
     def _set_squelch(self, sql):
-        return self._update("L SQL %s" % sql)
+        return self._update("L SQL %s" % self.sql)
 
     def _get_level(self):
-        return self._update("l")
+        return self._update("l STRENGTH")
 
     def _get_mode(self):
         return self._update('m')
+
+    def _scan(self):
+
+        if self.scan_start == True:
+            for key, values in self.freqs.items():
+                freq = key
+                mode = self.freqs[key]["mode"]
+                tag = self.freqs[key]["tag"]
+
+                self._set_freq(freq)
+                self._set_mode(mode)
+                self._set_squelch(self.sql)
+                time.sleep(0.2)
+                res = self._calc_RSSI()
+
+                if res >= self.signalStrength:
+                    print ("********************************************")
+                    time.sleep(0.1)
+                    while res >= self.signalStrength:
+                        print ("частота " + str(freq) + " уровень" + str(res))
+                        time.sleep(3.0)
+                        res = self._calc_RSSI()
+                else:
+                    print ("********************************************")
+                    print ("частота " + str(freq) + " уровень" + str(res))
+
+    def _calc_RSSI(self):
+        res = 0
+        for i in range(3):
+            RSSI = self._get_level()
+            res+=float(RSSI)
+            time.sleep(0.45)
+        res = res / 3
+        return int(res)
 
     def _update(self, msg):
         """
--- main.py
+++ main.py
@@ -2,70 +2,14 @@ from gqrx_scanner import*
 import csv
 import time
 
-def load_config(freq_csv='freq.csv'):
-    """
-    read the csv file with the frequencies & modes
-    in it into a dict{} where keys are the freq and
-    the value is a dict with the mode and a tag
-    """
-    freqs = {}
-    with open(freq_csv, 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter = ',')
-        for row in reader:
-            if int(row[3]) == 1:
-                freq = row[0]
-                freqs[freq] = {"mode":row[1],"tag": row[2]}
-    return freqs
-
-def tune_rec(freq, mode, tag, sql):
-
-    Rec._set_freq(freq)
-    Rec._set_mode(mode)
-    Rec._set_squelch(sql)
-
-def get_RSSI():
-
-    res = 0
-    for i in range(3):
-        RSSI = Rec._get_level()
-        res+=float(RSSI)
-        time.sleep(0.45)
-    res = res / 3
-    return int(res)
-    
-def update_screen():
-
-    return 0
-
 if __name__ == "__main__":
 
-    freq_set  = load_config()
-
     Rec = Receiver()
-    key_flag = 0
-    
+    Rec.scan_start = True
+
     while(1):
-        for key, values in freq_set.items():
-            freq = key
-            mode = freq_set[key]["mode"]
-            tag = freq_set[key]["tag"]
-            sql = -30
-
-            tune_rec(freq, mode, tag, sql)
-
-            time.sleep(0.2)
-
-            res =  get_RSSI()
-
-            if res >= sql:
-                time.sleep(0.2)
-                while res >= sql:
-                    print (res)
-                    time.sleep(3)
-                    res = get_RSSI()
-
-            else:
-                print (freq, res)
+        Rec._scan()
+        #Rec.scan_start = False
 
 
 
